d81146a37a2401cd7b64ff78b11fcd8567abfa7b
Joshua Greenfield <j.l.g.jobhunt@gmail.com>
2025-03-16 18:27:26 -0700

    More work on hatband_multiprocessing_mgmt.py planning out the work that can benefit from multiple CPU cores.

--- hatband_multiprocessing_mgmt.py
+++ hatband_multiprocessing_mgmt.py
@@ -56,19 +56,44 @@ def generate_list(beginning: float, end: float) -> list:
         output.append(i + math.rand(0.001))
     return output
 
-
-class HatbandCommunicator:
+def uncertain_work_activity(value: string):
+    print("Uncertain work activity.")
+    print("Value:")
+    print(f"{value}")
+    print("""
+          This is a placeholder for implementing functions like: 
+                1) HatbandCommunicator CLI Multiprocessing-Dependent Commands
+                    A) Hatband Record.key Generation - Single Key
+                    B) Hatband Record.key Generation - Batch Key Generation
+                       **Reason** Batch Key Generation can only be done in advance if
+                                  pseudorandom alphanumeric strings, int, or float.
+                    C) Hatband Record Search By Contents of Record.key
+                    D) Hatband Record Search By Contents of Record.value
+                    E) Dedicate Long-Running multiprocessing.Process to Writing JSONL Transaction Logs.
+                    F) Dedicate Long-Running multiprocessing.Process to Searching JSONL Transaction Logs.
+          
+                **Future** LLM Hatband CLI decipherment using NLTK. ** Future Feature **
+                **Reason** LLM can get command almost right very often.
+          """)
     
 
+
+class HatbandCommunicator:
+
     def __init__(self):
+
+        # Find the max number of of CPU cores usable by multiprocessing.
+        # 4 cores is a safe bet for all but duo-core.
+        # The numbers below ought be replaced with a CPU core detection function call.
+        MIN_CPU_CORES = 2
+        MAX_CPU_CORES = 4
 
         # generate_list(0.000, 1.001) is only default test data.
         self.default_value = generate_list(0.000, 1.001)
 
-
-    def work_queue(new_work_item: string) -> list:
+    def work_string_list_builder(new_work_item: string) -> list:
         """
-        work_queue is intended for the input of individual task assignments.
+        work_string_list_builder is intended for the input of individual task assignments.
         Each task assignment is a new_work_item.
         Each new_work_item is a string that contains a Hatband command in shorthand.
         """
@@ -76,9 +101,67 @@ class HatbandCommunicator:
 
         output.append(new_work_item)
 
-
+    @staticmethod
     def other_process(connection, values: list):
-        output = []
-        for value in values:
-            output.append()
     
+        for value in values:
+            
+            connection.send(uncertain_work_activity(value))
+            connection.close()
+
+    def batch_work_divider(max_cpu_cores, connection, work: list, beginning: int, end: int):
+        """
+        batch_work_divider is only useful and should only be called 
+        when dividing up large batches of work.
+
+            If not 0, beginning parameter should calculated from chunking
+            the list work when it is very large.
+
+            batch_work_divider would need to be called repeatedly on the 
+            incoming list with different beginning and end integers for list indices.
+        """
+            
+        if beginning == 0:
+            length = end + 1
+        elif beginning >= 1:
+            length = end - beginning
+
+        # This part is fragile and ought be more robust.
+        if length > max_cpu_cores and length % max_cpu_cores == 0:
+            worker_count = math.ceil(length / max_cpu_cores)
+        else:
+            worker_count = 1
+
+        processes = dict()
+        wd_cons = [0]
+        op_cons = [0]
+        combined = []
+        for i in range(1, worker_count + 1):
+            wd, op = Pipe()
+            wd_cons.append(wd)
+            op_cons.append(op)
+            processes.update({i: Process(target=HatbandCommunicator.other_process, args=(op_cons[i], beginning, beginning + max_cpu_cores))})
+            processes[i].start()
+            combined.extend(wd_cons[i].recv())
+            if i > 0:
+                beginning += max_cpu_cores
+
+        for i in range(1, worker_count + 1):
+            processes[i].join()
+
+        connection.send(combined)
+        connection.close()
+
+
+if __name__ == '__main__':
+    main_connection, other_connection = Pipe()
+
+    # beginning and end need to be replaced with the list length after being prepended
+    # with 0 in the 0 index.
+    beginning = 1
+    end = 101
+
+    other = Process(target=HatbandCommunicator.batch_work_divider, args=(other_connection, beginning, end))
+    other.start()
+    combined = main_connection.recv()
+    other.join()
